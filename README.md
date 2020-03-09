ce8f893472a9859203fdddf270dba924613e4881
Aida Perez <aida.capeca@gmai.com>
2020-03-09 17:12:55 -0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Profesional Profiles - App
-
-## Gettting Started 
-To initialize the project it's necesary to:
-1. Install NodeJS version 12.14.1
-2. Once instaled NodeJs and clone the repository go to the folder react-fundamentals and open GitBash 
-3. On GitBash console do npm install, this will download and install all th project dependencies. 
-4. Once npm install has finished do npm start, automatically this will open a browser with the http://localhost:3000/ 
---
+++ precache-manifest.93d43d85629e7d4d75dd4e0cc2d67947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df6b716d9c64ca08a6963e8e50584ea",
+    "url": "/np%20/index.html"
+  },
+  {
+    "revision": "466470547e9930d9b792",
+    "url": "/np%20/static/css/main.9017b840.chunk.css"
+  },
+  {
+    "revision": "a91ad2b5f3e3946cbe5f",
+    "url": "/np%20/static/js/2.805d5a17.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/np%20/static/js/2.805d5a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466470547e9930d9b792",
+    "url": "/np%20/static/js/main.8578d56c.chunk.js"
+  },
+  {
+    "revision": "04a2b04a9b1e6e8d9d52",
+    "url": "/np%20/static/js/runtime-main.27ebf48b.js"
+  }
+]);
